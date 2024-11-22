c4afd1d1bcd5b605a782a2234380ae0ae4fc339f
tepumasuta <thinkingocean2@gmail.com>
2024-11-22 16:39:34 +0000

    Changed `DeferSystem` interface

--- sem 3/task 11/include/defer.h
+++ sem 3/task 11/include/defer.h
@@ -3,6 +3,10 @@
 
 #include "arena.h"
 
+#ifndef DEFER_SYSTEM_DEFAULT_ENTRIES_CAP
+#define DEFER_SYSTEM_DEFAULT_ENTRIES_CAP 1024
+#endif
+
 typedef void (*defer_fn)(void *);
 typedef struct {
     defer_fn func;
@@ -10,17 +14,22 @@ typedef struct {
 } lambda_t;
 
 struct DeferSystem {
-    struct Arena *allocator;
-    struct lambda_t *entries;
+    struct ArenaDynamic *allocator;
+    ArenaOffset entries_base;
+    size_t entries_at;
+    bool own_allocator;
 };
 
-struct DeferSystem *defer_system_create(struct Arena *allocator);
+// Creates new arena with enough space to hold defer_system and capacity entries
+struct DeferSystem *defer_system_create(size_t capacity);
 void defer_system_destroy(struct DeferSystem **system);
-void defer_system_init(struct DeferSystem *system, struct Arena *allocator);
+// Allocator that is passed, it is consumed and owned, thus new allocations outside
+// may result in UB. It will not be destroyed or deinit in the end
+void defer_system_init(struct DeferSystem *system, struct ArenaDynamic *allocator, size_t capacity);
 void defer_system_deinit(struct DeferSystem *system);
 
 // Data may be NULL if unused
-bool defer_system_register(struct DeferSystem *system, lambda_t func);
+void defer_system_register(struct DeferSystem *system, lambda_t func);
 void defer_system_do_defer(struct DeferSystem *system);
 
 #endif
