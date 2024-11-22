efaaf299c574e9372f8509ea008e54d9a803dde9
tepumasuta <thinkingocean2@gmail.com>
2024-11-22 16:12:34 +0000

    Added `ArenaDynamic` interface and implementation

--- sem 3/task 11/include/arena.h
+++ sem 3/task 11/include/arena.h
@@ -11,37 +11,58 @@ struct ArenaStatic {
     bool alloced;
 };
 
+// Look-up index into arena relative to its base
+typedef ptrdiff_t ArenaOffset;
+// Expandable pointer unreliable arena
+struct ArenaDynamic {
+    void *memory;
+    size_t cap, at;
+};
+
 /* Heap memory allocation */
 struct ArenaStatic *arena_static_create(size_t capacity);
 struct ArenaStatic *arena_static_create_static_from_buffer(void *buffer, size_t buffer_capacity);
 void arena_static_destroy(struct ArenaStatic **arena);
+struct ArenaDynamic *arena_dynamic_create(size_t initial_capacity);
+void arena_dynamic_destroy(struct ArenaDynamic **arena);
 
 /* In-place memory allocation */
 void arena_static_init(struct ArenaStatic *arena, size_t capacity);
 // Doesn't have to be deallocated
 void arena_static_init_static_from_buffer(struct ArenaStatic *arena, void *buffer, size_t buffer_capacity);
 void arena_static_deinit(struct ArenaStatic *arena);
+void arena_dynamic_init(struct ArenaDynamic *arena, size_t initial_capacity);
+void arena_dynamic_deinit(struct ArenaDynamic *arena);
 
 
 /* Fast methods without checks */
 void *arena_static_alloc(struct ArenaStatic *arena, size_t size);
-// Assumes arena alloced
 void arena_static_reset(struct ArenaStatic *arena);
+ArenaOffset arena_dynamic_alloc(struct ArenaDynamic *arena, size_t size);
+void *arena_dynamic_get_memory(struct ArenaDynamic *arena, ArenaOffset offset);
+void arena_dynamic_reserve(struct ArenaDynamic *arena, size_t capacity);
+void arena_dynamic_reset(struct ArenaDynamic *arena);
 
 /* Info methods */
 size_t arena_static_capacity(struct ArenaStatic *arena);
 size_t arena_static_allocated(struct ArenaStatic *arena);
-// Whether was created from buffer
 bool arena_static_is_from_buffer(struct ArenaStatic *arena);  
+size_t arena_dynamic_capacity(struct ArenaDynamic *arena);
+size_t arena_dynamic_allocated(struct ArenaDynamic *arena);
 
 /* Safe methods with checks */
 struct ArenaStatic *arena_static_try_create(size_t capacity);
 struct ArenaStatic *arena_static_try_create_static_from_buffer(void *buffer, size_t buffer_capacity);
 bool arena_static_try_destroy(struct ArenaStatic **arena);
-bool arena_static_try_init(struct ArenaStatic *arena, size_t capacity);
+bool arena_static_try_init(struct ArenaStatic *arena, size_t initial_capacity);
 bool arena_static_try_init_static_from_buffer(struct ArenaStatic *arena, void *buffer, size_t buffer_capacity);
 bool arena_static_try_deinit(struct ArenaStatic *arena);
+struct ArenaDynamic *arena_dynamic_try_create(size_t initial_capacity);
+bool arena_dynamic_try_destroy(struct ArenaDynamic **arena);
 
 void *arena_static_try_alloc(struct ArenaStatic *arena, size_t size);
+bool arena_dynamic_try_alloc(struct ArenaDynamic *arena, size_t size, ArenaOffset *offset);
+void *arena_dynamic_try_get_memory(struct ArenaDynamic *arena, ArenaOffset offset);
+bool arena_dynamic_try_reserve(struct ArenaDynamic *arena, size_t capacity);
 
 #endif
--- sem 3/task 11/src/arena.c
+++ sem 3/task 11/src/arena.c
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #include <assert.h>
 
+static_assert(sizeof(char) == 1);
+
 struct ArenaStatic *arena_static_create(size_t capacity) {
     struct ArenaStatic *arena = malloc(sizeof(*arena) + sizeof(char) * capacity);
     *arena = (struct ArenaStatic){
@@ -170,3 +172,129 @@ void *arena_static_try_alloc(struct ArenaStatic *arena, size_t size) {
     arena->at += size;
     return mem;
 }
+
+struct ArenaDynamic *arena_dynamic_create(size_t initial_capacity) {
+    assert(initial_capacity > 0);
+    struct ArenaDynamic *arena = malloc(sizeof(*arena));
+    *arena = (struct ArenaDynamic){
+        .memory = malloc(sizeof(char) * initial_capacity),
+        .at = 0,
+        .cap = initial_capacity,
+    };
+    return arena;
+}
+
+void arena_dynamic_destroy(struct ArenaDynamic **arena) {
+    assert(arena);
+    assert(*arena);
+    free((*arena)->memory);
+    free(*arena);
+    *arena = NULL;
+}
+
+void arena_dynamic_init(struct ArenaDynamic *arena, size_t initial_capacity) {
+    assert(arena);
+    assert(initial_capacity > 0);
+    *arena = (struct ArenaDynamic){
+        .memory = malloc(sizeof(char) * initial_capacity),
+        .cap = initial_capacity,
+        .at = 0,
+    };
+}
+
+void arena_dynamic_deinit(struct ArenaDynamic *arena) {
+    assert(arena);
+    free(arena->memory);
+    *arena = (struct ArenaDynamic){
+        .memory = NULL,
+        .cap = 0,
+        .at = 0,
+    };
+}
+
+ArenaOffset arena_dynamic_alloc(struct ArenaDynamic *arena, size_t size) {
+    assert(arena);
+    while (arena->cap < arena->at + size) arena_dynamic_reserve(arena, arena->cap * 2);
+    ArenaOffset at = arena->at;
+    arena->at += size;
+    return at;
+}
+
+void *arena_dynamic_get_memory(struct ArenaDynamic *arena, ArenaOffset offset) {
+    assert(arena);
+    return &((char *)arena->memory)[offset];
+}
+
+void arena_dynamic_reserve(struct ArenaDynamic *arena, size_t capacity) {
+    assert(arena);
+    if (arena->cap >= capacity) return;
+    arena->memory = realloc(arena->memory, capacity);
+}
+
+void arena_dynamic_reset(struct ArenaDynamic *arena) {
+    assert(arena);
+    arena->at = 0;
+}
+
+size_t arena_dynamic_capacity(struct ArenaDynamic *arena) {
+    assert(arena);
+    return arena->cap;
+}
+
+size_t arena_dynamic_allocated(struct ArenaDynamic *arena) {
+    assert(arena);
+    return arena->at;
+}
+
+struct ArenaDynamic *arena_dynamic_try_create(size_t initial_capacity) {
+    assert(initial_capacity > 0);
+    struct ArenaDynamic *arena = malloc(sizeof(*arena));
+    if (!arena) return NULL;
+    *arena = (struct ArenaDynamic){
+        .memory = malloc(sizeof(char) * initial_capacity),
+        .at = 0,
+        .cap = initial_capacity,
+    };
+    if (!arena->memory) {
+        free(arena);
+        return NULL;
+    }
+    return arena;
+}
+
+bool arena_dynamic_try_destroy(struct ArenaDynamic **arena) {
+    assert(arena);
+    if (!*arena) return false;
+    free((*arena)->memory);
+    free(*arena);
+    *arena = NULL;
+    return true;
+}
+
+bool arena_dynamic_try_alloc(struct ArenaDynamic *arena, size_t size, ArenaOffset *offset) {
+    assert(arena);
+    assert(offset);
+    size_t new_cap = arena->cap;
+    while (new_cap < arena->at + size) new_cap *= 2;
+    if (new_cap != arena->cap && !arena_dynamic_try_reserve(arena, new_cap)) return false;
+    *offset = arena->at;
+    arena->at += size;
+    return true;
+}
+
+void *arena_dynamic_try_get_memory(struct ArenaDynamic *arena, ArenaOffset offset) {
+    assert(arena);
+    if (arena->cap <= offset) return NULL;
+    return &((char *)arena->memory)[offset];
+}
+
+bool arena_dynamic_try_reserve(struct ArenaDynamic *arena, size_t capacity) {
+    assert(arena);
+    if (arena->cap >= capacity) return true;
+    void *new_mem = realloc(arena, capacity);
+    if (!new_mem) return false;
+    arena->memory = new_mem;
+    arena->cap = capacity;
+    return true;
+}
+
