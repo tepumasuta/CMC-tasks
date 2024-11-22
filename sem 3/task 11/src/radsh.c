01912d572f52e64846d2d37d537e0a66bda05df2
tepumasuta <thinkingocean2@gmail.com>
2024-11-22 19:10:05 +0000

    Integrated defer system into main.c

--- sem 3/task 11/src/radsh.c
+++ sem 3/task 11/src/radsh.c
@@ -1,1 +1,19 @@
-int main(void) { return 69; }
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "arena.h"
+#include "defer.h"
+
+struct DeferSystem defer_system;
+
+void cleanup(void) {
+    defer_system_do_defer(&defer_system);
+    defer_system_deinit(&defer_system);
+}
+
+int main(void) {
+    if (!defer_system_try_init(&defer_system, NULL)) abort();
+    if (atexit(cleanup)) abort();
+
+    return 0;
+}
