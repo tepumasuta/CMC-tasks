fa6a7d121dcc77c9f95ae0e274d48adac96b943f
tepumasuta <thinkingocean2@gmail.com>
2024-11-24 18:03:47 +0000

    Reworked AST

--- sem 3/task 11/include/ast.h
+++ sem 3/task 11/include/ast.h
@@ -3,69 +3,55 @@
 
 #include "arena.h"
 
-
-struct NodeShell {
-    bool new_shell;
-    bool bg;
-    struct NodeConditional *command;
-    struct NodeShell *next;
-};
-
-struct NodeConditional {
-    struct NodeCommand *command;
-    bool if_false;
-    struct NodeConditional *next;
-};
-
-enum NodeCommandType {
-    NODE_COMMAND_IO_PIPE = 0,
-    NODE_COMMAND_I_PIPE_O,
-    NODE_COMMAND_PIPE_IO,
-    NODE_COMMAND_SHELL,
-    NODE_COMMAND_TYPE_SIZE,
-};
-
-struct NodeIOPipe {
-    char *input, *output;
-    bool append;
-    struct NodePipeline *pipe;
-};
-
-struct NodeCommand {
-    enum NodeCommandType type;
-    union {
-        struct NodeIOPipe *io_pipe;
-        struct NodeShell *command;
-    };
-};
-
-struct NodeBasicCommand {
-    char *name;
-    char **args;
-};
-
-struct NodePipeline {
-    struct NodeBasicCommand *command;
-    struct NodePipeline *pipe;
-};
-
 struct AST {
     struct NodeShell *root;
     struct ArenaStatic *string_allocator;
 };
 
+struct NodeShell {
+    bool new_shell;
+    bool bg;
+    char *input, *output;
+    bool append;
+    struct NodeConditional *command;
+    struct NodeShell *next;
+};
+
+struct NodeConditional {
+    struct NodePipeline *commands;
+    struct NodeConditional *next;
+    bool if_false;
+};
+
+enum NodeBasicity {
+    NODE_BASICITY_BASIC = 0,
+    NODE_BASICITY_SHELL,
+};
+
+struct NodePipeline {
+    enum NodeBasicity type;
+    union {
+        struct NodeBasicCommand *command;
+        struct NodeShell *shell;
+    };
+    struct NodePipeline *pipe;
+};
+
+struct NodeBasicCommand {
+    char *name;
+    char **args;
+    char *input, *output;
+    bool append;
+};
+
 void ast_node_shell_free(struct NodeShell *node);
 void ast_node_pipeline_free(struct NodePipeline *node);
 void ast_node_basic_command_free(struct NodeBasicCommand *node);
-void ast_node_command_free(struct NodeCommand *node);
-void ast_node_io_pipe_free(struct NodeIOPipe *node);
 void ast_node_conditional_free(struct NodeConditional *node);
 
 void ast_print_node_shell(struct NodeShell *node);
 void ast_print_node_pipeline(struct NodePipeline *node);
 void ast_print_node_basic_command(struct NodeBasicCommand *node);
-void ast_print_node_command(struct NodeCommand *node);
-void ast_print_node_io_pipe(struct NodeIOPipe *node);
 void ast_print_node_conditional(struct NodeConditional *node);
 
 #endif
--- sem 3/task 11/src/ast.c
+++ sem 3/task 11/src/ast.c
@@ -11,10 +11,20 @@ void ast_node_shell_free(struct NodeShell *node) {
     free(node);
 }
 
+void ast_node_conditional_free(struct NodeConditional *node) {
+    if (!node) return;
+    if (node->commands) ast_node_pipeline_free(node->commands);
+    if (node->next) ast_node_conditional_free(node->next);
+    free(node);
+}
+
 void ast_node_pipeline_free(struct NodePipeline *node) {
     if (!node) return;
-    if (node->command) ast_node_basic_command_free(node->command);
     if (node->pipe) ast_node_pipeline_free(node->pipe);
+    switch (node->type) {
+    case NODE_BASICITY_BASIC: ast_node_basic_command_free(node->command); break;
+    case NODE_BASICITY_SHELL: ast_node_shell_free(node->shell); break;
+    }
     free(node);
 }
 
@@ -23,40 +33,51 @@ void ast_node_basic_command_free(struct NodeBasicCommand *node) {
     free(node);
 }
 
-void ast_node_command_free(struct NodeCommand *node) {
-    if (!node) return;
-    assert(node->type != NODE_COMMAND_TYPE_SIZE);
-    if (node->type == NODE_COMMAND_SHELL) { if (node->command) ast_node_shell_free(node->command); }
-    else if (node->io_pipe) ast_node_io_pipe_free(node->io_pipe);
-    free(node);
-}
-
-void ast_node_io_pipe_free(struct NodeIOPipe *node) {
-    if (!node) return;
-    if (node->pipe) ast_node_pipeline_free(node->pipe);
-    free(node);
-}
-
-void ast_node_conditional_free(struct NodeConditional *node) {
-    if (!node) return;
-    if (node->next) ast_node_conditional_free(node->next);
-    if (node->command) ast_node_command_free(node->command);
-    free(node);
+static void print_string_or_nil(char *string){
+    if (!string) printf("nil");
+    else printf("`%s`", string);
 }
 
 void ast_print_node_shell(struct NodeShell *node) {
     if (!node) { printf("nil"); return; }
-    printf("NodeShell{new_shell=%d,bg=%d,command=", node->new_shell, node->bg);
+    printf("NodeShell{new_shell=%d,bg=%d,input=", node->new_shell, node->bg);
+    print_string_or_nil(node->input);
+    printf(",output=");
+    print_string_or_nil(node->output);
+    printf(",append=%d,command=", node->append);
     ast_print_node_conditional(node->command);
     printf(",next=");
     ast_print_node_shell(node->next);
     printf("}");
 }
 
+void ast_print_node_conditional(struct NodeConditional *node) {
+    if (!node) { printf("nil"); return; }
+    printf("NodeConditional{commands=");
+    ast_print_node_pipeline(node->commands);
+    if (node->next) {
+        printf(",next=");
+        ast_print_node_conditional(node->next);
+        printf(",if_false=%d", node->if_false);
+    } else {
+        printf(",next=nil");
+    }
+    printf("}");
+}
+
 void ast_print_node_pipeline(struct NodePipeline *node) {
     if (!node) { printf("nil"); return; }
-    printf("NodePipeline{command=");
-    ast_print_node_basic_command(node->command);
+    printf("NodePipeline{type=");
+    switch (node->type) {
+    case NODE_BASICITY_BASIC:
+        printf("BASIC,command=");
+        ast_print_node_basic_command(node->command);
+        break;
+    case NODE_BASICITY_SHELL:
+        printf("SHELL,shell=");
+        ast_print_node_shell(node->shell);
+        break;
+    }
     printf(",pipe=");
     ast_print_node_pipeline(node->pipe);
     printf("}");
@@ -67,53 +88,9 @@ void ast_print_node_basic_command(struct NodeBasicCommand *node) {
     printf("NodeBasicCommand{name=`%s`,args=[", node->name);
     for (char **p = node->args; *p; p++)
         printf("`%s`,", *p);
-    printf("]}");
-}
-
-void ast_print_node_command(struct NodeCommand *node) {
-    if (!node) { printf("nil"); return; }
-    printf("NodeCommand{type=");
-    switch (node->type) {
-    case NODE_COMMAND_IO_PIPE: printf("io_pipe"); break;
-    case NODE_COMMAND_I_PIPE_O: printf("i_pipe_o"); break;
-    case NODE_COMMAND_PIPE_IO: printf("pipe_io"); break;
-    case NODE_COMMAND_SHELL: printf("shell"); break;
-    case NODE_COMMAND_TYPE_SIZE: printf("type_size"); break;
-    }
-    if (node->type == NODE_COMMAND_TYPE_SIZE) { printf("}"); return; }
-    if (node->type == NODE_COMMAND_SHELL) {
-        printf(",command=");
-        ast_print_node_shell(node->command);
-        printf("}");
-        return;
-    }
-    printf(",io_pipe=");
-    ast_print_node_io_pipe(node->io_pipe);
-    printf("}");
-}
-
-void ast_print_node_io_pipe(struct NodeIOPipe *node) {
-    if (!node) { printf("nil"); return; }
-    printf("NodeIOPipe{input=");
-    if (node->input) printf("`%s`", node->input);
-    else printf("nil");
+    printf("],input=");
+    print_string_or_nil(node->input);
     printf(",output=");
-    if (node->output) printf("`%s`", node->output);
-    else printf("nil");
-    printf(",append=%d,pipe=", node->append);
-    ast_print_node_pipeline(node->pipe);
-    printf("}");
-}
-
-void ast_print_node_conditional(struct NodeConditional *node) {
-    if (!node) { printf("nil"); return; }
-    printf("NodeConditional{command=");
-    ast_print_node_command(node->command);
-    if (node->next) {
-        printf(",if_false=%d,next=", node->if_false);
-        ast_print_node_conditional(node->next);
-    } else {
-        printf(",next=nil");
-    }
-    printf("}");
+    print_string_or_nil(node->output);
+    printf(",append=%d}", node->append);
 }
