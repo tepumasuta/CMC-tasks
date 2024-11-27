0dfc33a0bba1e6fa9232e1f49b467a4676abc8b5
tepumasuta <thinkingocean2@gmail.com>
2024-11-27 16:51:33 +0000

    Removed unused token function

--- sem 3/task 11/include/token.h
+++ sem 3/task 11/include/token.h
@@ -46,7 +46,6 @@ typedef struct {
     size_t length;
 } token_view_t;
 
-size_t token_operator_precedence(enum TokenOperator op);
 bool token_is_redirection(enum TokenOperator op);
 
 void tv_chop(token_view_t *token_view);
