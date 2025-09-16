c92785636d38a8db6bde4585f995c40378edb88a
tepumasuta <thinkingocean2@gmail.com>
2025-09-17 00:33:19 +0300

    Shorten task7 to 101

--- sem 5/ml/task1/task7_try.py
+++ sem 5/ml/task1/task7_try.py
@@ -1,7 +1,7 @@
 # v1 = """
 def find_modified_max_argmax(L, f):
     L = tuple(f(v) for v in L if type(v) == int)
-    return L and max(enumerate(L), key=lambda x: x[1])[::-1]
+    return L and (max(L), L.index(max(L)))
 # """
 # remove_whitespace = lambda s: s.replace(' ','').replace('\n', '')
 
