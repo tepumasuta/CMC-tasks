7d8fc0e14c4ff121fb411155498dd822cbb5620a
tepumasuta <thinkingocean2@gmail.com>
2025-09-18 10:56:34 +0300

    Solved base task2 unit tests

---
+++ sem 5/ml/task2/base/base_functions.py
@@ -0,0 +1,33 @@
+from typing import List
+from copy import deepcopy
+
+
+def get_part_of_array(X: List[List[float]]) -> List[List[float]]:
+    """
+    X - двумерный массив вещественных чисел размера n x m. Гарантируется что m >= 500
+    Вернуть: двумерный массив, состоящий из каждого 4го элемента по оси размерности n 
+    и c 120 по 500 c шагом 5 по оси размерности m
+    """
+    return [line[120:500:5] for line in X[::4]]
+
+
+def sum_non_neg_diag(X: List[List[int]]) -> int:
+    """
+    Вернуть  сумму неотрицательных элементов на диагонали прямоугольной матрицы X. 
+    Если неотрицательных элементов на диагонали нет, то вернуть -1
+    """
+    X = tuple(X[i][i] for i in range(min(len(X), len(X[0]) if X else 0)) if X[i][i] >= 0)
+    return -1 if not X else sum(X)
+
+
+def replace_values(X: List[List[float]]) -> List[List[float]]:
+    """
+    X - двумерный массив вещественных чисел размера n x m.
+    По каждому столбцу нужно почитать среднее значение M.
+    В каждом столбце отдельно заменить: значения, которые < 0.25M или > 1.5M на -1
+    Вернуть: двумерный массив, копию от X, с измененными значениями по правилу выше
+    """
+    avgs = tuple(map(lambda x: x / len(X), map(sum, zip(*X))))
+    return [[val if M <= val * 4 <= M * 6 else -1
+             for M, val in zip(avgs, row)]
+            for row in X]
