d17d341b0b13bdb1359e834f0486549d469c2b7a
tepumasuta <thinkingocean2@gmail.com>
2026-02-15 01:30:37 +0300

    Fixed matmul test bug. Implemented `Matrix`'s m `__mul__`, `__getitem__`, `__eq__` & `__repr__`

--- sem 6/numerical methods/matrix.py
+++ sem 6/numerical methods/matrix.py
@@ -1,5 +1,6 @@
 from typing import Iterable
 import copy
+import itertools
 
 
 class Matrix[T]:
@@ -12,7 +13,7 @@ class Matrix[T]:
         if not all(len(row) == self.__cols for row in self.__raw):
             assert False, "TODO: raise ValueError"
 
-    def copy(self) -> "Matrix":
+    def copy(self) -> "Matrix[T]":
         return Matrix(copy.deepcopy(self.__raw))
 
     @property
@@ -23,9 +24,38 @@ class Matrix[T]:
     def cols(self) -> int:
         return self.__cols
 
+    def __mul__(self, other) -> "Matrix[T]":
+        if type(other) is Matrix:
+            if self.cols != other.rows:
+                assert False, "TODO: raise ValueError"
+            return Matrix(
+                (sum(self[i, k] * other[k, j] for k in range(self.cols)) for j in range(other.cols))
+                for i in range(self.rows)
+            )
+        return NotImplemented
+
+    def __getitem__(self, index: tuple[int, int]) -> T:
+        i, j = index
+        if i not in range(self.rows) or j not in range(self.cols):
+            assert False, "TODO: raise KeyError"
+        return self.__raw[i][j]
+
+    def __eq__(self, other) -> bool:
+        if not isinstance(other, Matrix):
+            return False
+        return (
+            self.cols == other.cols
+            and self.rows == other.rows
+            and all(self[i, j] == other[i, j] for i, j in itertools.product(range(self.rows), range(self.cols)))
+        )
+
+    def __repr__(self) -> str:
+        max_spacing = max(len(str(v)) for row in self.__raw for v in row)
+        return f'{'\n'.join(f'[{' '.join(str(v).ljust(max_spacing)for v in row)}]' for row in self.__raw)}'
+
 
 def test_matmul():
-    assert Matrix([[1, 2], [3, 4]]) * Matrix([[5, 6], [7, 8]]) == Matrix([[19, 43], [22, 50]])
+    assert Matrix([[1, 2], [3, 4]]) * Matrix([[5, 6], [7, 8]]) == Matrix([[19, 22], [43, 50]])
     assert Matrix([[1, 2], [3, 4]]) * Matrix([[1, 0], [0, 1]]) == Matrix([[1, 2], [3, 4]])
     assert Matrix([[1, 0], [0, 1]]) * Matrix([[1, 2], [3, 4]]) == Matrix([[1, 2], [3, 4]])
     assert Matrix([[1, 2], [3, 4]]) * Matrix([[1], [0]]) == Matrix([[1], [3]])
