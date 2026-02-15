496cfc324ed899fc7be11b7974acada0480ab991
tepumasuta <thinkingocean2@gmail.com>
2026-02-15 21:56:47 +0300

    Added `test_determinant_recursive`, `test_coordination`, decoupled common part of tests into `basic_test`

--- sem 6/numerical methods/determinant.py
+++ sem 6/numerical methods/determinant.py
@@ -9,7 +9,7 @@ import abc
 
 class Determinant(abc.ABC):
     @abc.abstractmethod
-    def calculate[T](cls, mat: Matrix[T]) -> T: ...
+    def calculate[T](self, mat: Matrix[T]) -> T: ...
 
 
 class DeterminantStupid(Determinant):
@@ -23,23 +23,44 @@ class DeterminantStupid(Determinant):
         return 1 if sum(perm[i] > perm[j] for i in range(len(perm)) for j in range(i + 1, len(perm))) % 2 == 0 else -1
 
 
-def test_determinant_combinatorical():
-    assert Matrix.eye(3).det(DeterminantStupid()) == 1
-    assert Matrix.eye(2).det(DeterminantStupid()) == 1
-    assert Matrix.eye(1).det(DeterminantStupid()) == 1
+class DeterminantRecursive(Determinant):
+    def calculate[T](self, mat: Matrix[T]) -> T:
+        return self.rec(mat, size=mat.cols)
+
+    def rec[T](self, mat: Matrix[T], size: int) -> T: ...
+
+
+def basic_test(determinant: Determinant):
+    assert Matrix.eye(3).det(determinant) == 1
+    assert Matrix.eye(2).det(determinant) == 1
+    assert Matrix.eye(1).det(determinant) == 1
 
     for _ in range(10):
         diag = random_gen.generate_random_diagonal_matrix(size=5, low=1, value_type=int)
         upper = random_gen.generate_random_upper_triangular_matrix(size=5, low=1, value_type=int)
         lower = random_gen.generate_random_lower_triangular_matrix(size=5, low=1, value_type=int)
-        assert diag.det(DeterminantStupid()) == math.prod(diag[i, i] for i in range(diag.cols))
-        assert upper.det(DeterminantStupid()) == math.prod(upper[i, i] for i in range(diag.cols))
-        assert lower.det(DeterminantStupid()) == math.prod(lower[i, i] for i in range(diag.cols))
+        assert diag.det(determinant) == math.prod(diag[i, i] for i in range(diag.cols))
+        assert upper.det(determinant) == math.prod(upper[i, i] for i in range(diag.cols))
+        assert lower.det(determinant) == math.prod(lower[i, i] for i in range(diag.cols))
         diag = random_gen.generate_random_diagonal_matrix(size=5, high=-1, value_type=int)
         upper = random_gen.generate_random_upper_triangular_matrix(size=5, high=-1, value_type=int)
         lower = random_gen.generate_random_lower_triangular_matrix(size=5, high=-1, value_type=int)
-        assert diag.det(DeterminantStupid()) == math.prod(diag[i, i] for i in range(diag.cols))
-        assert upper.det(DeterminantStupid()) == math.prod(upper[i, i] for i in range(diag.cols))
-        assert lower.det(DeterminantStupid()) == math.prod(lower[i, i] for i in range(diag.cols))
+        assert diag.det(determinant) == math.prod(diag[i, i] for i in range(diag.cols))
+        assert upper.det(determinant) == math.prod(upper[i, i] for i in range(diag.cols))
+        assert lower.det(determinant) == math.prod(lower[i, i] for i in range(diag.cols))
     mat, det = random_gen.generate_mixed_matrix_with_known_determinant(size=9, span=range(1, 5))
-    assert mat.det(DeterminantStupid()) == det
+    assert mat.det(determinant) == det
+
+
+def test_determinant_combinatorical():
+    basic_test(DeterminantStupid())
+
+
+def test_determinant_recursive():
+    basic_test(DeterminantRecursive())
+
+
+def test_coordination():
+    for _ in range(10):
+        mat, _ = random_gen.generate_mixed_matrix_with_known_determinant(size=7, span=range(1, 5))
+        assert mat.det(DeterminantStupid()) == mat.det(DeterminantRecursive())
