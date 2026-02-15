8c5d71c930a8495855167594635001e46bfde872
tepumasuta <thinkingocean2@gmail.com>
2026-02-15 23:41:00 +0300

    Added `test_random_vec`. Implemented `generate_random_vec`

--- sem 6/numerical methods/random_gen.py
+++ sem 6/numerical methods/random_gen.py
@@ -191,6 +191,53 @@ def generate_mixed_matrix_with_known_determinant[T](
     return m, det
 
 
+def generate_random_vec[T](*, length=None, low=None, high=None, span: None | range = None, value_type=None):
+    if span is not None and (low is not None or high is not None or value_type is not None):
+        assert False, "TODO: raise ValueError"
+    if value_type is None:
+        t = type(low)
+        if t is type(None) or type(high) is float:
+            t = type(high)
+        value_type = t
+
+    # TODO: Make better check
+    # if low is not None and not issubclass(value_type, type(low)):
+    #     assert False, "TODO: raise ValueError"
+    # if high is not None and not issubclass(value_type, type(high)):
+    #     assert False, "TODO: raise ValueError"
+
+    cols = 1
+    rows = length
+
+    if rows <= 0 or cols <= 0:
+        assert False, "TODO: raise ValueError"
+
+    if span is not None:
+        linearized = iter(random.choices(span, k=length))
+    elif value_type is float:
+        if low is None:
+            low = 0.0
+        if high is None:
+            high = 1.0
+        if low > high:
+            assert False, "TODO: raise ValueError"
+        linearized = (random.random() * (high - low) + low for _ in range(rows))
+    elif value_type is int:
+        if high is None:
+            high = 2**64
+        if low is None:
+            low = -(2**64)
+        if low > high:
+            assert False, "TODO: raise ValueError"
+        linearized = (random.randint(low, high) for i in range(rows))
+    else:
+        assert False, "TODO: raise ValueError"
+    return matrix.Matrix.vec(linearized, convert_to_fractions=False)
+
+
+# def generate_random_linear_system[T](size: int, )
+
+
 def test_random_matrices():
     seeds = random.choices(range(2_781_443_143_657_574), k=10**5)
     state = random.getstate()
@@ -456,5 +503,44 @@ def test_random_diagonal_matrix():
             assert mat.cols == mat.rows == size
             assert mat.generic_type is int
             assert mat.is_diag()
+
+    random.setstate(state)
+
+
+def test_random_vec():
+    seeds = random.choices(range(2_781_443_143_657_574), k=10**5)
+    state = random.getstate()
+
+    seeds_iter = iter(seeds)
+
+    for _, seed in zip(range(1000), seeds_iter):
+        random.seed(seed)
+        for size in range(1, 6):
+            mat = generate_random_vec(length=size, low=-1000, high=1000)
+            assert all(-1000 <= mat[i, 0] <= 1000 for i in range(size))
+            assert mat.cols == 1 and mat.rows == size
+            assert mat.generic_type is int
+        for size in range(1, 6):
+            low, high = random.randint(-(10**5), 10**5), random.randint(-(10**5), 10**5)
+            low, high = min(low, high), max(low, high)
+            mat = generate_random_vec(length=size, low=low, high=high)
+            assert all(low <= mat[i, 0] <= high for i in range(size))
+            assert mat.cols == 1 and mat.rows == size
+            assert mat.generic_type is int
+        for size in range(1, 6):
+            mat = generate_random_vec(length=size, low=-666, high=15.0)
+            assert all(-666 <= mat[i, 0] <= 15.0 for i in range(size))
+            assert mat.cols == 1 and mat.rows == size
+            assert mat.generic_type == float
+        for size in range(1, 6):
+            mat = generate_random_vec(length=size, value_type=float)
+            assert all(0.0 <= mat[i, 0] <= 1.0 for i in range(size))
+            assert mat.cols == 1 and mat.rows == size
+            assert mat.generic_type == float
+        for size in range(1, 6):
+            mat = generate_random_vec(length=size, span=range(-100, 200, 5))
+            assert all(-100 <= mat[i, 0] <= 200 and mat[i, 0] % 5 == 0 for i in range(size))
+            assert mat.cols == 1 and mat.rows == size
+            assert mat.generic_type is int
 
     random.setstate(state)
