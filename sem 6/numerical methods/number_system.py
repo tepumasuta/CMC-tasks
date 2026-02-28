d1eb4195b09a73646095a64cd6161ca41d526b1d
tepumasuta <thinkingocean2@gmail.com>
2026-03-01 02:45:07 +0300

    Removed inappropriate check from test

--- sem 6/numerical methods/number_system.py
+++ sem 6/numerical methods/number_system.py
@@ -279,7 +279,6 @@ def test_int_constructors():
     assert Int() == Int()
     assert Int() == int()
     assert Int(69) == 69
-    assert 69 == Int(69)
     assert Int(69.0) == Int(69)
     assert Int(Fraction(42)) == Int(42)
 
